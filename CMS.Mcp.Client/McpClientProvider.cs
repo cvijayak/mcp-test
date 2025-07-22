8dec435a9865602f528dcc8b0c1e04ce04c2c29c
Chandrasekar Vijayakumar <chandrasekar.vijayakumar@viewpoint.com>
2025-07-22 22:06:33 +0530

    Fixed the spinner issue in the GetMcpTools page

--- CMS.Mcp.Client/McpClientProvider.cs
+++ CMS.Mcp.Client/McpClientProvider.cs
@@ -28,18 +28,9 @@
                 var sessionProvider = _serviceProvider.GetRequiredService<ISessionProvider>();
                 
                 var endpoint = serverOptions.Value.GetSseUri();
-                var authInjectingTransport = new McpSseTransport(endpoint, "TrimbleCMS", sessionProvider);
+                var authInjectingTransport = new McpSseTransport(endpoint, "MonkeyMcpClientTool", sessionProvider);
 
-                var mcpClient = await McpClientFactory.CreateAsync(authInjectingTransport);
-                var kernel = _serviceProvider.GetRequiredService<Kernel>();
-
-                var tools = await mcpClient.ListToolsAsync();
-
-#pragma warning disable SKEXP0001
-                kernel.Plugins.AddFromFunctions("MonkeyMcpClientTool", tools.Select(t => t.AsKernelFunction()));
-#pragma warning restore SKEXP0001
-
-                return mcpClient;
+                return await McpClientFactory.CreateAsync(authInjectingTransport);
             });
         }
 
@@ -57,17 +48,20 @@
 
         public async Task<FunctionResult> InvokePromptAsync(string message)
         {
-            var client = await _clientTask.Value;
-
 #pragma warning disable SKEXP0001
-            var execSettings = new OpenAIPromptExecutionSettings 
+            var client = await _clientTask.Value;
+            var kernel = _serviceProvider.GetRequiredService<Kernel>();
+            if (!kernel.Plugins.Contains("MonkeyMcpClientTool"))
+            {
+                var tools = await client.ListToolsAsync();
+                kernel.Plugins.AddFromFunctions("MonkeyMcpClientTool", tools.Select(t => t.AsKernelFunction()));
+            }
+
+            return await kernel.InvokePromptAsync(message, new KernelArguments(new OpenAIPromptExecutionSettings 
             {
                 Temperature = 0,
                 FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(options: new() { RetainArgumentTypes = true })
-            };
-
-            var kernel = _serviceProvider.GetRequiredService<Kernel>();
-            return await kernel.InvokePromptAsync(message, new KernelArguments(execSettings));
+            }));
 #pragma warning restore SKEXP0001
         }
     }
