6b86e7cd6cb2102206eac0e758934163102cad3b
Chandrasekar Vijayakumar <chandrasekar.vijayakumar@viewpoint.com>
2025-07-22 15:58:20 +0530

    Fixed some UI related issues

--- CMS.Mcp.Server.Contracts/Monkey.cs
+++ CMS.Mcp.Server.Contracts/Monkey.cs
@@ -1,10 +1,10 @@
 ﻿namespace CMS.Mcp.Server.Contracts
 {
     public class Monkey {
-        public string? Name { get; set; }
-        public string? Location { get; set; }
-        public string? Details { get; set; }
-        public string? Image { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
         public int Population { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
